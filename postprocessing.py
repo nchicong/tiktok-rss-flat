36faecef398de28b6a5e6a93b713ec7aca3805f7
CONGNC6 <cong.nguyen@onemount.com>
2021-08-08 00:00:40 +0700

    test html

--- postprocessing.py
+++ postprocessing.py
@@ -55,8 +55,8 @@ with urllib.request.urlopen("https://jsonblob.com/api/jsonBlob/283f6f37-f78c-11e
             fe.published(datetime.fromtimestamp(tiktok['createTime'], timezone.utc))
             fe.title(tiktok['desc'])
             fe.link(href=link)
-            fe.description("<html><body><a href='" + link + "'><img src='" + tiktok['video']['cover'] + "' /></a><a href='" + link + "'>Link</a></body></html>")
-            fe.content(content=str("<html><body><a href='" + link + "'><img src='" + tiktok['video']['cover'] + "' /></a><a href='" + link + "'>Link</a></body></html>"), type="html")
+            fe.description("<![CDATA[<a href='" + link + "'><img src='" + tiktok['video']['originCover'] + "' /></a><a href='" + link + "'>Link</a>]]>")
+            # fe.content(content=str("<html><body><a href='" + link + "'><img src='" + tiktok['video']['originCover'] + "' /></a><a href='" + link + "'>Link</a></body></html>"), type="html")
 
         fg.rss_file('rss/' + user + '.xml') # Write the RSS feed to a file
 
