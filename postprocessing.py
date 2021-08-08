b722849af517487ec6ebcd8f86ed1ad9bf74e8c0
CONGNC6 <cong.nguyen@onemount.com>
2021-08-08 21:44:28 +0700

    change back to cdata

--- postprocessing.py
+++ postprocessing.py
@@ -56,7 +56,7 @@ with urllib.request.urlopen("https://jsonblob.com/api/jsonBlob/283f6f37-f78c-11e
             fe.published(datetime.fromtimestamp(tiktok['createTime'], timezone.utc))
             fe.title(tiktok['desc'])
             fe.link(href=link)
-            fe.description(escape("<![CDATA[<a href='" + link + "'><img src='" + tiktok['video']['originCover'] + "' /></a><a href='" + link + "'>Link</a>]]>"))
+            fe.description("<![CDATA[<a href='" + link + "'><img src='" + tiktok['video']['originCover'] + "' /></a><br/><a href='" + link + "'>Link</a>]]>")
             # fe.content(content=str("<html><body><a href='" + link + "'><img src='" + tiktok['video']['originCover'] + "' /></a><a href='" + link + "'>Link</a></body></html>"), type="html")
 
         fg.rss_file('rss/' + user + '.xml') # Write the RSS feed to a file
